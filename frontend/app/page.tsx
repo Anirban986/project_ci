a72e6f53d39350a8090330835ddcfbc4c6be786e
Anirban986 <chakrabortyanirban228@gmail.com>
2026-05-03 23:19:03 +0530

    added auto-merge and auto-pr workflow

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,4 @@
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold">hello this is my new app</h1>;
+  return <h1 className="text-3xl font-bold">hello welcome to my new app</h1>;
 }
