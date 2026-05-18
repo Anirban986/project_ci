72ae2bff563c60e31e2ae3058945d781a9f071f9
Anirban Chakraborty <chakrabortyanirban228@gmail.com>
2026-05-18 17:32:07 +0530

    Feature/final fix (#10)
    * removed wrong api.js
    
    * added email service

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -12,7 +12,9 @@ const { sendForgotPasswordOTP } = require("../utils/mailer");
 dotenv.config();
 // Email setup
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
