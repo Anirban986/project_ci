0375df2e5f1bfbfaf1bb8d8049623796429e99d6
Anirban986 <chakrabortyanirban228@gmail.com>
2026-05-18 14:54:03 +0530

    updated api.ts

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -10,7 +10,7 @@ import axios from "axios";
 */
 
 const api = axios.create({
-  baseURL: process.env.BACKEND_URI || "http://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URI || "http://localhost:5000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
