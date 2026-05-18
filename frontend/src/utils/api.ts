af3422e28ac97b52c323b0d5832a9959f9b8f28d
Anirban986 <chakrabortyanirban228@gmail.com>
2026-05-18 15:44:07 +0530

    addressed server connection issue

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -10,11 +10,15 @@ import axios from "axios";
 */
 
 const api = axios.create({
+  
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URI || "http://localhost:5000",
+  
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+console.log("API BASE URL:", process.env.NEXT_PUBLIC_BACKEND_URL);
 
 export default api;
