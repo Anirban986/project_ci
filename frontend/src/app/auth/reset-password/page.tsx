e92dc3dc6b8ade580450e38dd8a6304b8baf6b3f
Anirban986 <chakrabortyanirban228@gmail.com>
2026-05-18 12:58:20 +0530

    fix frontend issue

--- frontend/src/app/auth/reset-password/page.tsx
+++ frontend/src/app/auth/reset-password/page.tsx
@@ -1,8 +1,13 @@
+import { Suspense } from "react";
 import { ResetPasswordPage } from "@/src/features/auth/Resetpasswordpage";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = { title: "Reset Password" };
 
 export default function Page() {
-  return <ResetPasswordPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPasswordPage />
+    </Suspense>
+  );
 }
